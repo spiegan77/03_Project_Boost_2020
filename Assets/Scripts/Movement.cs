b56ef2a97b91f10d96807f0033f9991cdf41cc19
Steve_U57 <obsidian77@gmail.com>
2021-03-10 19:00:48 -0500

    36_Using_AddRelativeForce()

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Pressed Space - Rotate Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Pressed Space - Rotate Right");
+        }
+    }
+
+    private void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up);
+        }
+    }
+}
