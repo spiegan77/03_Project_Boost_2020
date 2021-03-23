3ee6705a394ef8abd4ab6a0da9ecf9910a83b461
Steve_U57 <obsidian77@gmail.com>
2021-03-23 06:25:13 -0400

    61_Wrap_Up_Project_Boost

---
+++ Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApplication : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+            {
+                Debug.Log("You Hit ESC");
+                Application.Quit();
+            }
+    }
+}
